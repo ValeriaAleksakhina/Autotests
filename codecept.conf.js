a2a16d307f1ad2adf603ff2ed1ec7fecac354527
Valeria_Aleksakhina <Virago400@mail.ru>
2023-08-05 08:47:17 +0400

    added tests

---
+++ codecept.conf.js
@@ -0,0 +1,16 @@
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://testing-dev.zocean.pw/ui/autotest_js_snippet_requirede033358e1f244813b546d0c4edab97f7/04f7d09f-1768-495a-865a-06e7a64811ac#vIn=true&cId=root',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'exampleTests'
+}
